74dab69a26bb0d4540d5961ef0e6e0ac4c5992c7
jtmccutcheon <jt.mccutcheon@live.com>
2020-01-22 14:24:17 -0500

    mvp

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -18,11 +18,38 @@ def selection_sort(arr):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-
+    n = len(arr)
+   # Traverse through all array elements
+    for i in range(n):
+            # Last i elements are already in correct position
+        for j in range(0, n-i-1):
+                # Swap if the element found is greater than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    ############################################################################
+    # for n in range(len(arr)-1, 0, -1):
+    #     for i in range(n):
+    #         if arr[i] > arr[i+1]:
+    #             temp = arr[i]
+    #             arr[i] = arr[i+1]
+    #             arr[i+1] = temp
+    ##############################################################################################
+    # exchanges = True
+    # passnum = len(arr)-1
+    # while passnum > 0 and exchanges:
+    #     exchanges = False
+    #     for i in range(passnum):
+    #         if arr[i] > arr[i+1]:
+    #             exchanges = True
+    #             temp = arr[i]
+    #             arr[i] = arr[i+1]
+    #             arr[i+1] = temp
+    #     passnum = passnum-1
     return arr
 
+    # STRETCH: implement the Count Sort function below
 
-# STRETCH: implement the Count Sort function below
+
 def count_sort(arr, maximum=-1):
 
     return arr
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,45 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
-    
+    # delcare indicies for each sublist
+    a = 0
+    b = 0
+    for i in range(elements):
+        # check if either list is empty if so append the other
+        if a >= len(arrA):
+            merged_arr[i] = arrB[b]
+            b += 1
+        elif b >= len(arrB):
+            merged_arr[i] = arrA[a]
+            a += 1
+        # otherwise compare and append the smallest of the two
+        elif arrA[a] < arrB[b]:
+            merged_arr[i] = arrA[a]
+            a += 1
+        else:
+            merged_arr[i] = arrB[b]
+            b += 1
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+def merge_sort(arr):
     # TO-DO
-
-    return arr
+    # check if length is 1 or less if so return list
+    if len(arr) <= 1:
+        return arr
+    # divide in half
+    left = arr[0:len(arr) // 2]
+    right = arr[len(arr) // 2]
+    # sort left
+    merge_sort(left)  # bubble or quicksort can work here too
+    # sort right
+    merge_sort(right)
+    # merge together
+    return merge(left, right)
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -20,7 +48,8 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -28,6 +57,6 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
